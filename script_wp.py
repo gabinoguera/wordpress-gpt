c4c012413a11ff4d85b92571d464fafd2b498a33
gabi.noguera <68296070+gabinoguera@users.noreply.github.com>
2023-07-25 10:56:47 +0200

    Update script_wp.py V2
    se cambia el modelo text-davinci por el modelo de chat gpt-3.5-turbo-16k

--- script_wp.py
+++ script_wp.py
@@ -7,10 +7,10 @@ from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
 import os
 
-# Leemos el archivo CSV que subimos al entorno de Colab(el encoding que me ha funcionado es Latin pero se puede probar con utf-8)
+# Leemos el archivo CSV que subimos al directorio(el encoding que me ha funcionado es Latin pero se puede probar con utf-8)
 
-df = pd.read_csv('tomate_edicion.csv', encoding='latin-1')
-#df = pd.read_csv('question.csv', encoding='utf-8')
+df = pd.read_csv('tu_archivo.csv', encoding='latin-1')
+#df = pd.read_csv('tu_archivo.csv', encoding='utf-8')
 
 # Cargar las variables de entorno desde el archivo .env
 from dotenv import load_dotenv, find_dotenv
@@ -31,25 +31,25 @@ for i, row in df.iterrows():
 
 question =', '.join(questions)
 
-consulta= f"""Eres historiador, especializado en la historia del tomate en España.\
-Responde con un articulo de 700 palabras las siguientes preguntas: {question}.\
+prompt= f""" Responde con un articulo de 2500 palabras las siguientes preguntas: {question}.\
 Hazlo de manera divertida, dinámica, que llame a ser leído por todo el público amante del tomate.\
 El articulo debe contener una estructura HTML con subtitulos en H2.\
-Respalda la información con datos históricos, estadísticos o relevantes.\
-Incluye información de wikipedia con links a los articulos\
+No inventes la información. Respalda con datos históricos, estadísticos o relevantes citando la fuente de información.\
 No des conclusiones, solo datos objetivos\
 """
+
 try:
-  response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=consulta,
-    max_tokens=3800,
-    temperature=0.3,
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-16k",
+    messages=[
+    { "role": "system", "content": "Eres historiador experto en la historia del tomate en España" },
+    { "role": "user", "content": prompt} ],
+    max_tokens=5000,
+    temperature=0,
   )
 except Exception as e:
   print("Error al realizar la solicitud a la API de OpenAI:", str(e))
   exit()
-
 
 
 # RELLENAR: Establecer la URL de la API de WordPress y los encabezados de autorización
@@ -61,14 +61,19 @@ headers = {
 
 # Establecer el cuerpo de la solicitud con el título y el estado del post(en este caso lo verás en "draft" pero puedes cambiarlo si lo prefieres a "publish" para su publicación automática)
 data = {
-    'title': "El tomate en toda su diversidad: Descubre los tipos y sus características",
-    'content': response["choices"][0]["text"],
+    'title': "Aquí va el título que tendrá tu artículo",
+    'content': response['choices'][0]['message']['content'],
     'status': 'draft'
 }
 
 # Realizar la solicitud POST
 response = requests.post(url, headers=headers, json=data)
 
+# Verificar el estado de la respuesta
+if response.status_code == 201:
+    print('Post creado correctamente')
+else:
+    print('Error al crear el post')
 # Verificar el estado de la respuesta
 if response.status_code == 201:
     print('Post creado correctamente')
